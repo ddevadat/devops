c85c45ce099e1019695adcc2a8cb192e1f90d957
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-08-29 14:25:58 +1000

    script update
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- scripts/deploy_sunbird-rc.sh
+++ scripts/deploy_sunbird-rc.sh
@@ -4,4 +4,4 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 script_path=$(pwd)
 jobName=sunbird-rc
-ansible-playbook -i ${ANSIBLE_INVENTORY_LOCATION}/hosts ../ansible/deploy_core_service.yml --extra-vars \"chart_path=../helm_charts/$jobName release_name=$jobName image_tag=${image_tag} image_name=${image_name} role_name=helm-deploy"
+ansible-playbook -i ${ANSIBLE_INVENTORY_LOCATION}/hosts ../ansible/deploy_core_service.yml --extra-vars \"chart_path=../helm_charts/$jobName release_name=$jobName image_tag=${image_tag} image_name=${image_name} role_name=helm-deploy\"
