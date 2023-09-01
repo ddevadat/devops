2b8b351ea9a2c5fecf760ef059b623370a987be5
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-09-01 17:13:42 +1000

    loop
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- scripts/deploy_sunbird-rc.sh
+++ scripts/deploy_sunbird-rc.sh
@@ -4,10 +4,8 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 script_path=$(pwd)
 file_path="services.lst"
-# Deploy certificateapi
-while IFS= read -r line; do
-    echo "Line: $line"
-    # ansible-playbook -i ${ANSIBLE_INVENTORY_LOCATION}/hosts ../ansible/deploy_core_service.yml --extra-vars "chart_path=../helm_charts/$jobName release_name=$jobName role_name=helm-deploy"
 
-done < "$file_path"
-
+for jobName in `cat $file_path`
+do
+   ansible-playbook -i ${ANSIBLE_INVENTORY_LOCATION}/hosts ../ansible/deploy_core_service.yml --extra-vars "chart_path=../helm_charts/$jobName release_name=$jobName role_name=helm-deploy"
+done
