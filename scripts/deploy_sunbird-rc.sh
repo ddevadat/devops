42eb3fab1c4b71b04af5cdd0e014b14397cc6674
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-09-01 16:25:22 +1000

    testing individual chart
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- scripts/deploy_sunbird-rc.sh
+++ scripts/deploy_sunbird-rc.sh
@@ -3,5 +3,7 @@ source ./env.sh
 bold=$(tput bold)
 normal=$(tput sgr0)
 script_path=$(pwd)
-jobName=sunbird-rc
+
+# Deploy certificateapi
+jobName=certificateapi
 ansible-playbook -i ${ANSIBLE_INVENTORY_LOCATION}/hosts ../ansible/deploy_core_service.yml --extra-vars "chart_path=../helm_charts/$jobName release_name=$jobName role_name=helm-deploy"
