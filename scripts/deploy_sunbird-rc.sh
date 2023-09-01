dd066ce9798da5773cb3f386e659f282d947757e
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-09-01 16:51:25 +1000

    testing iterative
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- scripts/deploy_sunbird-rc.sh
+++ scripts/deploy_sunbird-rc.sh
@@ -6,4 +6,5 @@ script_path=$(pwd)
 
 # Deploy certificateapi
 jobName=certificateapi
-ansible-playbook -i ${ANSIBLE_INVENTORY_LOCATION}/hosts ../ansible/deploy_core_service.yml --extra-vars "chart_path=../helm_charts/$jobName release_name=$jobName role_name=helm-deploy"
+# ansible-playbook -i ${ANSIBLE_INVENTORY_LOCATION}/hosts ../ansible/deploy_core_service.yml --extra-vars "chart_path=../helm_charts/$jobName release_name=$jobName role_name=helm-deploy"
+ansible-playbook -i ${ANSIBLE_INVENTORY_LOCATION}/hosts ../ansible/deploy_core_service.yml role_name=helm-deploy"
