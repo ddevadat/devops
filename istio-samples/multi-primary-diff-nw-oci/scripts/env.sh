dce3e091b4a28354a6d4777dd97fc7dfb0ca71b3
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-02-14 02:52:42 +0000

    updated istio version default
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- istio-samples/multi-primary-diff-nw-oci/scripts/env.sh
+++ istio-samples/multi-primary-diff-nw-oci/scripts/env.sh
@@ -2,7 +2,7 @@ log() { echo "$1" >&2; }
 
 cluster1_oci_profile_name="REGION1"
 cluster2_oci_profile_name="REGION2"
-ISTIO_VERSION=${ISTIO_VERSION:=1.18.2}
+ISTIO_VERSION=${ISTIO_VERSION:=1.19.0}
 ISTIO_INSTALL_LOCATION=/tmp/istio-${ISTIO_VERSION}
 ISTIO_NAMESPACE=istio-system
 CTX_CLUSTER1=oke_cluster1
